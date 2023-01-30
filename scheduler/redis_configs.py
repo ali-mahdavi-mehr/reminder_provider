0c63f541920968ec778fda61778812db323b25b3
Alima <aliali.ali1378@yahoo.com>
2023-01-30 13:04:36 +0330

    bug fix

--- scheduler/redis_configs.py
+++ scheduler/redis_configs.py
@@ -4,5 +4,6 @@ from reminder_provider.settings import env
 
 
 def get_redis_db(db=0):
-    redis_db = Redis.from_url(f'{env("REDIS_URL")}/{db}')
+    redis_db = Redis.from_url(env("REDIS_URL"), db=db)
+    return redis_db
 
